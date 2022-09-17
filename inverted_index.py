a04c1ad1dd03a5c96e70a6f21682251f6c11744e
Alyson Givre <alysong24@gmail.com>
2022-09-17 16:10:00 -0400

    Clean up InvertedIndex

--- inverted_index.py
+++ inverted_index.py
@@ -5,28 +5,38 @@ class InvertedIndex:
     def add_word(self, document_id: int, word: str):
         """Add a word to the index."""
         if word in self.index.keys():
-            self._add_existing_word(document_id, word)
+            self.__add_existing_word(document_id, word)
         else:
-            self._add_new_word(document_id, word)
+            self.__add_new_word(document_id, word)
 
-    def _add_existing_word(self, document_id: int, word: str):
-        """Add a word that already exists in the index."""
-        doc_list = self.index[word]["doc_list"]
+    def __add_existing_word(self, document_id: int, word: str):
+        """Add a word that already exists in the index.
 
-        if document_id not in doc_list.keys():
-            doc_list[document_id] = 1
+        :param document_id: The ID of the document being processed
+        :param word: The word in the index that should be updated
+        :return: None
+        """
+        postings_list = self.index[word]["doc_list"]
+
+        if document_id not in postings_list.keys():
+            postings_list[document_id] = 1
             self.index[word]["num_docs"] += 1
         else:
-            doc_list[document_id] += 1
+            postings_list[document_id] += 1
 
         self.index[word]["count"] += 1
 
-    def _add_new_word(self, document_id: int, word: str):
-        """Add a new word to the index."""
-        doc_list = {"size": 1, document_id: 1}
+    def __add_new_word(self, document_id: int, word: str):
+        """Add a new word to the index.
+
+        :param document_id: The ID of the document being processed
+        :param word: The new word to be added
+        :return: None
+        """
+        postings_list = {"size": 1, document_id: 1}
 
         self.index[word] = {
             "count": 1,
             "num_docs": 1,
-            "doc_list": doc_list
+            "postings_list": postings_list
         }
--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from indexer import Indexer
 def main():
     for dataset_name in ["yelp", "headlines"]:
         dataset_path = f"./sample_data/{dataset_name}.txt"
-        parser = Indexer(dataset_path, dataset_name)
+        indexer = Indexer(dataset_path, dataset_name)
 
-        parser.calculate_metrics()
-        parser.find_singleton_words()
-        parser.find_frequencies()
+        indexer.calculate_metrics()
+        indexer.find_singleton_words()
+        indexer.find_frequencies()
 
 
 if __name__ == '__main__':
