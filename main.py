734105ae7a3e5c9e5cc1b2821857096b4575e7e0
Alyson Givre <alysong24@gmail.com>
2022-12-21 15:14:48 -0500

    Set up FastAPI server and add query endpoint

--- main.py
+++ main.py
@@ -1,32 +1,14 @@
-import time
+from dotenv import load_dotenv
+from fastapi import FastAPI
 
-from index.indexer import Indexer
-from index.inverted_index import InvertedIndex
-from index.processor import Processor
-
-
-def main():
-    start = time.time()
-
-    for dataset_name in ["animal"]:
-        dataset_path = f"./sample_data/{dataset_name}.txt"
-        processor = Processor()
-        index = InvertedIndex()
-
-        indexer = Indexer(dataset_path, dataset_name, processor, index)
-        indexer.load_data()
-
-        lexicon_file, inverted_file, document_length_file = index.generate_file(dataset_name)
-
-        indexer.calculate_metrics()
-        indexer.find_singleton_words()
-        indexer.find_frequencies()
-
-    end = time.time()
-
-    print(f"Seconds elapsed: {end - start}")
-    print(f"Minutes elapsed: {(end - start) / 60}")
+from logging_tools.config import setup_logging
 
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+app = FastAPI()
+setup_logging()
+
+
+from routers import query  # noqa: E402
+
+app.include_router(query.router)
